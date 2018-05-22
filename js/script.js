bda3d7bcc7034aca50eb3abbde7a5ba7fb44c6e2
Anita Lew Kiedrowska <anitalk@poczta.onet.pl>
2018-05-22 09:27:28 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 function drawTree(h) {
-  for (var i = 0; i < h; i++) {
-    var star = '';
-    for (var j = 0; j <= h - i; j++) {
-      star += ' ';
-    }
-    for (var x = 0; x < i * 2 - 1; x++) {
-      star += '*';
-    }
-    console.log(star);
-  }
+	for (var i = 0; i < h; i++) {
+		var star = '';
+
+		for (var j = 0; j < h - i; j++) {
+			star += ' ';
+		}
+
+		for (var j = 0; j < i * 2 + 1; j++) {
+			star += '*';
+		}
+
+		console.log(star);
+	}
 }
 
 drawTree(prompt('How many rows a tree is to have?'));
